c975ef33d5da9652846526adffecbf8dc2c9b89e
Melih Anıl Eroğlu <100762394+melihanileroglu@users.noreply.github.com>
2022-10-14 12:14:37 +0300

    Create query_11.sql

---
+++ query_11.sql
@@ -0,0 +1,71 @@
+--1)actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+
+(
+    SELECT first_name  FROM actor
+    ORDER BY first_name
+)
+UNION
+(
+SELECT first_name  FROM actor
+    ORDER BY first_name 
+);
+
+--2)actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+(
+    SELECT first_name FROM actor
+    ORDER BY first_name
+)
+INTERSECT
+(
+    SELECT first_name FROM customer
+    ORDER BY first_name 
+);
+
+--3)actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+
+
+(
+    SELECT first_name FROM actor
+    ORDER BY first_name
+)
+EXCEPT
+(
+    SELECT first_name FROM customer
+    ORDER BY first_name 
+);
+--4)İlk 3 sorguyu tekrar eden veriler için de yapalım.
+-- 1.A
+(
+    SELECT first_name  FROM actor
+    ORDER BY first_name
+)
+UNION ALL
+(
+SELECT first_name  FROM actor
+    ORDER BY first_name 
+);
+
+--2.A
+
+(
+    SELECT first_name FROM actor
+    ORDER BY first_name
+)
+INTERSECT ALL
+(
+    SELECT first_name FROM customer
+    ORDER BY first_name 
+);
+
+--3.A
+
+(
+    SELECT first_name FROM actor
+    ORDER BY first_name
+)
+EXCEPT ALL
+(
+    SELECT first_name FROM customer
+    ORDER BY first_name 
+);
